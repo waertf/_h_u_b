bef7d57b401c6c656b3e99a953221f8359901ce8
alonso <alonso@wavegis.com.tw>
2014-03-13 14:30:14 +0800

    initial hub project

---
+++ hub/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace hub
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.kmPerHour = new System.Windows.Forms.Label();
+            this.speedLabel = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.persent = new System.Windows.Forms.Label();
+            this.batteryLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.taskLabel = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.kmPerHour);
+            this.panel1.Controls.Add(this.speedLabel);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 200);
+            this.panel1.TabIndex = 0;
+            // 
+            // kmPerHour
+            // 
+            this.kmPerHour.AutoSize = true;
+            this.kmPerHour.Font = new System.Drawing.Font("PMingLiU", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.kmPerHour.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.kmPerHour.Location = new System.Drawing.Point(295, 81);
+            this.kmPerHour.Name = "kmPerHour";
+            this.kmPerHour.Size = new System.Drawing.Size(105, 43);
+            this.kmPerHour.TabIndex = 1;
+            this.kmPerHour.Text = "km/h";
+            // 
+            // speedLabel
+            // 
+            this.speedLabel.AutoSize = true;
+            this.speedLabel.Font = new System.Drawing.Font("PMingLiU", 150F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.speedLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.speedLabel.Location = new System.Drawing.Point(-34, 0);
+            this.speedLabel.Name = "speedLabel";
+            this.speedLabel.Size = new System.Drawing.Size(369, 200);
+            this.speedLabel.TabIndex = 0;
+            this.speedLabel.Text = "000";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.persent);
+            this.panel2.Controls.Add(this.batteryLabel);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(0, 200);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 200);
+            this.panel2.TabIndex = 1;
+            // 
+            // persent
+            // 
+            this.persent.AutoSize = true;
+            this.persent.Font = new System.Drawing.Font("PMingLiU", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.persent.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.persent.Location = new System.Drawing.Point(319, 82);
+            this.persent.Name = "persent";
+            this.persent.Size = new System.Drawing.Size(54, 43);
+            this.persent.TabIndex = 2;
+            this.persent.Text = "%";
+            // 
+            // batteryLabel
+            // 
+            this.batteryLabel.AutoSize = true;
+            this.batteryLabel.Font = new System.Drawing.Font("PMingLiU", 150F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.batteryLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.batteryLabel.Location = new System.Drawing.Point(-32, 0);
+            this.batteryLabel.Name = "batteryLabel";
+            this.batteryLabel.Size = new System.Drawing.Size(369, 200);
+            this.batteryLabel.TabIndex = 2;
+            this.batteryLabel.Text = "000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "label2";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.taskLabel);
+            this.panel3.Location = new System.Drawing.Point(0, 400);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(400, 200);
+            this.panel3.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("PMingLiU", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label3.Location = new System.Drawing.Point(295, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 43);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Task";
+            // 
+            // taskLabel
+            // 
+            this.taskLabel.AutoSize = true;
+            this.taskLabel.Font = new System.Drawing.Font("PMingLiU", 150F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.taskLabel.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.taskLabel.Location = new System.Drawing.Point(-32, 3);
+            this.taskLabel.Name = "taskLabel";
+            this.taskLabel.Size = new System.Drawing.Size(369, 200);
+            this.taskLabel.TabIndex = 3;
+            this.taskLabel.Text = "000";
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(400, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(400, 300);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(400, 300);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(400, 300);
+            this.panel5.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label speedLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label kmPerHour;
+        private System.Windows.Forms.Label batteryLabel;
+        private System.Windows.Forms.Label persent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label taskLabel;
+
+    }
+}
+
---
+++ hub/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace hub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var autoSendFromSqlTableTimer =
+                    new System.Timers.Timer((int)uint.Parse(ConfigurationManager.AppSettings["autorequest_interval"]));
+            autoSendFromSqlTableTimer.Elapsed += (sender, e) => { WebServiceRequest(); };
+            autoSendFromSqlTableTimer.Enabled = true;
+
+            //Thread webServiceRequesThread = new Thread(() => WebServiceRequest());
+            //webServiceRequesThread.Start();
+        }
+
+        private void WebServiceRequest()
+        {
+            Chilkat.Xml xml = new Chilkat.Xml();
+            xml.LoadXmlFile("data.xml");
+            // Navigate to the first company record.
+            xml.FirstChild2();
+            string gps_long = xml.GetChildContent("gps_long");
+            string gps_lat = xml.GetChildContent("gps_lat");
+            string speed = xml.GetChildContent("speed");
+            string electricity = xml.GetChildContent("electricity");
+            xml.NextSibling2();
+            xml.FirstChild2();
+            string roadtype1 = xml.GetChildContent("roadtype");
+            xml.NextSibling2();
+            string roadtype2 = xml.GetChildContent("roadtype");
+            Invoke((MethodInvoker)delegate
+            {
+                speedLabel.Text = speed;
+                batteryLabel.Text = electricity;
+            });
+
+            //start to send to avls server
+            TcpClient avlsTcpClient = null;
+            NetworkStream networkStream = null;
+            try
+            {
+                string Temp = "NA";
+                string Status = "00000000";
+                string time = DateTime.UtcNow.ToString("yyMMddHHmmss");
+                string Speed = speed;
+                string Dir = "0";
+                string uid = "0";
+                string gps = "A";
+                string _event = "0";
+                string message = "null";
+                string loc = "null";
+                string package = "%%" + uid + "," +
+                              gps + "," +
+                              time + "," +
+                              loc + "," +
+                              Speed + "," +
+                              Dir + "," +
+                              Temp + "," +
+                              Status + "," +
+                              _event + "," +
+                              message + Environment.NewLine;
+                byte[] sendByte = Encoding.UTF8.GetBytes(package);
+                avlsTcpClient = new TcpClient(ConfigurationManager.AppSettings["AVLS_SERVER_IP"],
+                    int.Parse(ConfigurationManager.AppSettings["AVLS_SERVER_PORT"]));
+                networkStream = avlsTcpClient.GetStream();
+                networkStream.Write(sendByte, 0, sendByte.Length);
+                networkStream.Flush();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+            finally
+            {
+                if(networkStream!=null)
+                    networkStream.Close();
+                if(avlsTcpClient!=null)
+                    avlsTcpClient.Close();
+            }
+            
+
+        }
+    }
+}

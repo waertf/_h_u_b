215300900996ef4e5f1fb77ff82059c2df21276f
alonso <alonso@wavegis.com.tw>
2014-03-14 11:17:31 +0800

    initial texbox project

---
+++ textBox/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace textBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSpeed = new System.Windows.Forms.TextBox();
+            this.textBoxBattery = new System.Windows.Forms.TextBox();
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.textBoxDirection = new System.Windows.Forms.TextBox();
+            this.textBoxRoadType1 = new System.Windows.Forms.TextBox();
+            this.textBoxRoadType2 = new System.Windows.Forms.TextBox();
+            this.textBoxRoadType3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxSpeed
+            // 
+            this.textBoxSpeed.Location = new System.Drawing.Point(116, 12);
+            this.textBoxSpeed.Name = "textBoxSpeed";
+            this.textBoxSpeed.Size = new System.Drawing.Size(100, 22);
+            this.textBoxSpeed.TabIndex = 0;
+            this.textBoxSpeed.Text = "Speed";
+            // 
+            // textBoxBattery
+            // 
+            this.textBoxBattery.Location = new System.Drawing.Point(116, 40);
+            this.textBoxBattery.Name = "textBoxBattery";
+            this.textBoxBattery.Size = new System.Drawing.Size(100, 22);
+            this.textBoxBattery.TabIndex = 1;
+            this.textBoxBattery.Text = "Battery";
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(116, 68);
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTask.TabIndex = 2;
+            this.textBoxTask.Text = "Task";
+            // 
+            // textBoxDirection
+            // 
+            this.textBoxDirection.Location = new System.Drawing.Point(116, 100);
+            this.textBoxDirection.Name = "textBoxDirection";
+            this.textBoxDirection.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDirection.TabIndex = 3;
+            this.textBoxDirection.Text = "Direction";
+            // 
+            // textBoxRoadType1
+            // 
+            this.textBoxRoadType1.Location = new System.Drawing.Point(116, 127);
+            this.textBoxRoadType1.Name = "textBoxRoadType1";
+            this.textBoxRoadType1.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRoadType1.TabIndex = 4;
+            this.textBoxRoadType1.Text = "RoadType1";
+            // 
+            // textBoxRoadType2
+            // 
+            this.textBoxRoadType2.Location = new System.Drawing.Point(116, 155);
+            this.textBoxRoadType2.Name = "textBoxRoadType2";
+            this.textBoxRoadType2.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRoadType2.TabIndex = 5;
+            this.textBoxRoadType2.Text = "RoadType2";
+            // 
+            // textBoxRoadType3
+            // 
+            this.textBoxRoadType3.Location = new System.Drawing.Point(116, 183);
+            this.textBoxRoadType3.Name = "textBoxRoadType3";
+            this.textBoxRoadType3.Size = new System.Drawing.Size(100, 22);
+            this.textBoxRoadType3.TabIndex = 6;
+            this.textBoxRoadType3.Text = "RoadType3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 12);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "textBoxSpeed";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "textBoxBattery";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 12);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "textBoxTask";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 12);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "textBoxDirection";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 12);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "textBoxRoadType1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(97, 12);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "textBoxRoadType2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 194);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(97, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "textBoxRoadType3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxRoadType3);
+            this.Controls.Add(this.textBoxRoadType2);
+            this.Controls.Add(this.textBoxRoadType1);
+            this.Controls.Add(this.textBoxDirection);
+            this.Controls.Add(this.textBoxTask);
+            this.Controls.Add(this.textBoxBattery);
+            this.Controls.Add(this.textBoxSpeed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSpeed;
+        private System.Windows.Forms.TextBox textBoxBattery;
+        private System.Windows.Forms.TextBox textBoxTask;
+        private System.Windows.Forms.TextBox textBoxDirection;
+        private System.Windows.Forms.TextBox textBoxRoadType1;
+        private System.Windows.Forms.TextBox textBoxRoadType2;
+        private System.Windows.Forms.TextBox textBoxRoadType3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ textBox/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace textBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var autoSendFromSqlTableTimer =
+                    new System.Timers.Timer((int)uint.Parse(ConfigurationManager.AppSettings["autorequest_interval"]));
+            autoSendFromSqlTableTimer.Elapsed += (sender, e) => { WebServiceRequest(); };
+            autoSendFromSqlTableTimer.Enabled = true;
+        }
+        private void WebServiceRequest()
+        {
+            Chilkat.Xml xml = new Chilkat.Xml();
+            xml.Encoding = "utf-8";
+            xml.LoadXmlFile("data.xml");
+            // Navigate to the first company record.
+            xml.FirstChild2();
+            string gps_long = xml.GetChildContent("gps_long");
+            string gps_lat = xml.GetChildContent("gps_lat");
+            string speed = xml.GetChildContent("speed");
+            string electricity = xml.GetChildContent("electricity");
+            string direction = xml.GetChildContent("direction");
+            xml.NextSibling2();
+            int roadCount = xml.NumChildrenHavingTag("record");
+            xml.FirstChild2();
+            List<string> roadTypeList = new List<string>();
+            for (int i = 0; i < roadCount; i++)
+            {
+                roadTypeList.Add(xml.GetChildContent("roadtype"));
+                xml.NextSibling2();
+            }
+            xml.Dispose();
+            //string roadtype1 = xml.GetChildContent("roadtype");
+            //xml.NextSibling2();
+            //string roadtype2 = xml.GetChildContent("roadtype");
+            this.InvokeEx(f => f.textBoxSpeed.Text = speed);
+            this.InvokeEx(f => f.textBoxBattery.Text = electricity);
+            this.InvokeEx(f => f.textBoxDirection.Text = direction);
+            for (int i = 0; i < roadTypeList.Count(); i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        this.InvokeEx(f => f.textBoxRoadType1.Text = roadTypeList[0]);
+                        break;
+                    case 1:
+                        this.InvokeEx(f => f.textBoxRoadType2.Text = roadTypeList[1]);
+                        break;
+                    case 2:
+                        this.InvokeEx(f => f.textBoxRoadType3.Text = roadTypeList[2]);
+                        break;
+                }
+            }
+            this.InvokeEx(f => f.Invalidate());
+            this.InvokeEx(f => f.Update());
+
+
+            //start to send to avls server
+            TcpClient avlsTcpClient = null;
+            NetworkStream networkStream = null;
+
+            try
+            {
+                string lat_str = gps_lat, long_str = gps_long;
+                ConvertLocToAvlsLoc(ref lat_str, ref long_str);
+                string Temp = "NA";
+                string Status = "00000000";
+                string time = DateTime.UtcNow.ToString("yyMMddHHmmss");
+                string Speed = speed;
+                string Dir = "0";
+                string uid = "0";
+                string gps = "A";
+                string _event = "0";
+                string message = "null";
+                string loc = "N" + lat_str + "E" + long_str;
+                string package = "%%" + uid + "," +
+                              gps + "," +
+                              time + "," +
+                              loc + "," +
+                              Speed + "," +
+                              Dir + "," +
+                              Temp + "," +
+                              Status + "," +
+                              _event + "," +
+                              message + Environment.NewLine;
+                byte[] sendByte = Encoding.UTF8.GetBytes(package);
+                avlsTcpClient = new TcpClient(ConfigurationManager.AppSettings["AVLS_SERVER_IP"],
+                    int.Parse(ConfigurationManager.AppSettings["AVLS_SERVER_PORT"]));
+                networkStream = avlsTcpClient.GetStream();
+                BufferedStream bs = new BufferedStream(networkStream);
+                bs.Write(sendByte, 0, sendByte.Length);
+                bs.Flush();
+                //networkStream.Write(sendByte, 0, sendByte.Length);
+                //networkStream.Flush();
+            }
+            catch (Exception ex)
+            {
+
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                if (networkStream != null)
+                    networkStream.Close();
+                if (avlsTcpClient != null)
+                    avlsTcpClient.Close();
+            }
+
+
+        }
+        private void ConvertLocToAvlsLoc(ref string lat, ref string lon)
+        {
+            double tmpLat = double.Parse(lat);
+            double tmpLon = double.Parse(lon);
+            double latInt = Math.Truncate(tmpLat);
+            double lonInt = Math.Truncate(tmpLon);
+            double latNumberAfterPoint = tmpLat - latInt;
+            double lonNumberAfterPoint = tmpLon - lonInt;
+            lat = ((latNumberAfterPoint * 60 / 100 + latInt) * 100).ToString();
+            lon = ((lonNumberAfterPoint * 60 / 100 + lonInt) * 100).ToString();
+        }
+    }
+    public static class ISynchronizeInvokeExtensions
+    {
+        public static void InvokeEx<T>(this T @this, Action<T> action) where T : ISynchronizeInvoke
+        {
+            if (@this.InvokeRequired)
+            {
+                @this.Invoke(action, new object[] { @this });
+            }
+            else
+            {
+                action(@this);
+            }
+        }
+    }
+}

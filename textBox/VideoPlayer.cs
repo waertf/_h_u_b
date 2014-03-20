f1c9960f9f7c8ec709dd9e3ff0c6fe9ccd09399d
nb <waertf.register@m2k.com.tw>
2014-03-20 11:53:39 +0800

    add video play/stop function

--- textBox/VideoPlayer.cs
+++ textBox/VideoPlayer.cs
@@ -17,12 +17,17 @@ namespace textBox
             axWindowsMediaPlayer1.URL = AppDomain.CurrentDomain.BaseDirectory + "test.wmv";
             axWindowsMediaPlayer1.Ctlcontrols.play();
             axWindowsMediaPlayer1.uiMode = "none";
-            this.Load += new EventHandler(VideoPlayer_Load);
+            
         }
 
-        void VideoPlayer_Load(object sender, EventArgs e)
+        public void Play()
         {
-           
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+        }
+
+        public void Stop()
+        {
+            axWindowsMediaPlayer1.Ctlcontrols.stop();
         }
     }
 }

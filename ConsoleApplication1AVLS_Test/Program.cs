d9376278bc239eb51fb5b757c1d6d73602401f9c
nb <waertf.register@m2k.com.tw>
2014-03-20 17:42:51 +0800

    combine byte array finish

--- ConsoleApplication1AVLS_Test/Program.cs
+++ ConsoleApplication1AVLS_Test/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleApplication1AVLS_Test
             List<sbyte> avlSbytes = new List<sbyte>();
             try
             {
-                
                 avlSbytes.Add(-88);
                 avlSbytes.Add(-88);
                 avlSbytes.Add(-128);
@@ -31,6 +30,23 @@ namespace ConsoleApplication1AVLS_Test
                 {
                     avlSbytes.Add(48);
                 }
+                avlSbytes.Add(69);
+                string lat = "121.522894";
+                ConvertLocation(avlSbytes, lat);
+                avlSbytes.Add(78);
+                string lon = "25.063547";
+                ConvertLocation(avlSbytes, lon);
+                for (int i = 0; i < 5; i++)
+                {
+                    avlSbytes.Add(0);
+                }
+                sbyte crc=avlSbytes[0];
+                for (int i = 1; i < avlSbytes.Count; i++)
+                {
+                    crc ^= avlSbytes[i];
+                }
+                avlSbytes.Add(crc);
+                avlSbytes.Add(13);
             }
             catch (Exception ex)
             {
@@ -42,6 +58,18 @@ namespace ConsoleApplication1AVLS_Test
                 byte[] unsigned = new byte[avlSbytes.Count];
                 Buffer.BlockCopy(avlSbytes.ToArray(), 0, unsigned, 0, avlSbytes.Count);
             }
+        }
+
+        private static void ConvertLocation(List<sbyte> avlSbytes, string lat)
+        {
+            string[] result = lat.Split(new char[] { '.' });
+            string firstResult = result[1].Substring(0, 2);
+            string secondResult = result[1].Substring(2, 2);
+            string thridResult = result[1].Substring(4, 2);
+            avlSbytes.Add((sbyte)(Convert.ToByte(result[0])));
+            avlSbytes.Add((sbyte)(Convert.ToByte(firstResult)));
+            avlSbytes.Add((sbyte)(Convert.ToByte(secondResult)));
+            avlSbytes.Add((sbyte)(Convert.ToByte(thridResult)));
         }
     }
 }
